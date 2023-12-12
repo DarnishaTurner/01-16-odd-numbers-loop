76c2cdaa7de6925d16ea6a6f5f240e7762c9ae3b
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-11 19:25:27 -0500

    Darnishas finished copy

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const endNum = 10; // Change this number to test with different ranges
 
 // TODO: Use a do-while loop to iterate and log odd numbers within the range
 
+do {
+    if(i % 2 !== 0) {
+        console.log(i);
+    }
+    i++;
+} while (i <= endNum);
